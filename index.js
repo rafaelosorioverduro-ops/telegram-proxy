2cb70116ab2dc7d9e3aae384736520b31d6945f3
rafaelosorioverduro-ops <rafaelosorioverdu.ro@gmail.com>
2026-03-16 09:43:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
+const  TOKEN = "8572280228:AAHZ_gN0pJ8MMZ5EjNOoz7u5HGXsLyFFGo4";
+const const CHAT_ID = "8231324366";
+
+app.get("/send.php", async (req, res) => {
+  const texto = req.query.msg || "Mensaje vacío";
+
+  const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: texto,
+      }),
+    });
+
+    res.send("OK");
+  } catch (e) {
+    res.status(500).send("Error");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
